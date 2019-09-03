98e581cbcc94f7745e465c5597a544c6941c75a2
Joao Pucci <joao.pucci@live.com>
2019-09-03 02:07:04 -0300

    Add swipe refresh for event list

--- app/src/main/java/tech/pucci/checkthis/ui/activity/EventsActivity.kt
+++ app/src/main/java/tech/pucci/checkthis/ui/activity/EventsActivity.kt
@@ -1,9 +1,14 @@
 package tech.pucci.checkthis.ui.activity
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -14,33 +19,70 @@ import tech.pucci.checkthis.model.Event
 
 class EventsActivity : AppCompatActivity() {
 
-    lateinit var rvEvents: RecyclerView
+    private lateinit var srlEvents: SwipeRefreshLayout
+    private lateinit var rvEvents: RecyclerView
+    private lateinit var ivErrorLoading: ImageView
+    private lateinit var tvErrorLoading: TextView
+
     private val events: ArrayList<Event> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_events)
 
+        ivErrorLoading = findViewById(R.id.events_recycler_view_error_placeholder)
+        tvErrorLoading = findViewById(R.id.events_recycler_view_error_text)
+        srlEvents = findViewById(R.id.events_swipe_layout)
+        srlEvents.setOnRefreshListener { requestEvents() }
+
         rvEvents = findViewById(R.id.events_recycler_view)
         rvEvents.adapter = EventsAdapter(this, events)
+    }
 
+    override fun onResume() {
+        super.onResume()
         requestEvents()
     }
 
     private fun requestEvents() {
         RetrofitInitializer().eventsService().get().enqueue(object : Callback<List<Event>?> {
             override fun onFailure(call: Call<List<Event>?>, t: Throwable) {
-                Toast.makeText(this@EventsActivity, getString(R.string.error_request_events), Toast.LENGTH_LONG)
-                    .show()
                 t.printStackTrace()
+
+                updateEventList(listOf())
+
+                ivErrorLoading.setImageDrawable(ContextCompat.getDrawable(this@EventsActivity, R.drawable.ic_refresh_blue_24dp))
+                tvErrorLoading.text = getString(R.string.error_loading_event_list)
             }
 
             override fun onResponse(call: Call<List<Event>?>, response: Response<List<Event>?>) {
                 response.body()?.let {
-                    events.addAll(it)
-                    rvEvents.adapter?.notifyDataSetChanged()
+                    updateEventList(it)
+
+                    if(events.isEmpty()) {
+                        ivErrorLoading.setImageDrawable(ContextCompat.getDrawable(this@EventsActivity, R.drawable.ic_sentiment_dissatisfied_blue_24dp))
+                        tvErrorLoading.text = getString(R.string.event_list_no_events)
+                    }
                 }
             }
         })
+    }
+
+    private fun updateEventList(eventList: List<Event>) {
+        refreshEvents(eventList)
+        srlEvents.isRefreshing = false
+        showListPlaceholderIf(events.isEmpty())
+    }
+
+    private fun refreshEvents(refreshedEvents: List<Event>) {
+        events.clear()
+        events.addAll(refreshedEvents)
+        rvEvents.adapter?.notifyDataSetChanged()
+    }
+
+    private fun showListPlaceholderIf(shouldShow: Boolean) {
+        ivErrorLoading.visibility = if(shouldShow) View.VISIBLE else View.GONE
+        tvErrorLoading.visibility = ivErrorLoading.visibility
+        rvEvents.visibility = if(shouldShow) View.GONE else View.VISIBLE
     }
 }
